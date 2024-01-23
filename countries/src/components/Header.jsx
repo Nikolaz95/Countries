ca97688fc6300eb7ab31461993a83c3cf6bf134b
Nikola Zovko <nikolajoe95@gmail.com>
2024-01-23 21:14:56 +0100

    novo

--- countries/src/components/Header.jsx
+++ countries/src/components/Header.jsx
@@ -9,7 +9,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='headerContent'>
-      <h1>Where in the world ? 111</h1>
+      <h1>Where in the world ?</h1>
     </div>
   )
 }
